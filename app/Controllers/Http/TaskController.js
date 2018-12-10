1cdaa717abd3b46982f0c57e11c47aa97174dea2
sudhir meena <sureshmeena512@gmail.com>
2018-12-10 19:09:07 +0530

    minor fix

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -2,21 +2,21 @@
 const Task = use('App/Models/Task')
 class TaskController {
     async home({view}) {
-        const task1 = new Task
-        task1.title = 'Walk the dog'
-        task1.description = 'Go and walk for half an hour'
-        task1.done = false
-        const task2 = new Task
-        task2.title = 'Feed the cat'
-        task2.description = 'The food is on the fridge'
-        task2.done = false
-        const task3 = new Task
-        task3.title = 'Buy some pizza'
-        task3.description = 'Go at the shop before 7pm'
-        task3.done = true
-        await task1.save()
-        await task2.save()
-        await task3.save()
+        // const task1 = new Task
+        // task1.title = 'Walk the dog'
+        // task1.description = 'Go and walk for half an hour'
+        // task1.done = false
+        // const task2 = new Task
+        // task2.title = 'Feed the cat'
+        // task2.description = 'The food is on the fridge'
+        // task2.done = false
+        // const task3 = new Task
+        // task3.title = 'Buy some pizza'
+        // task3.description = 'Go at the shop before 7pm'
+        // task3.done = true
+        // await task1.save()
+        // await task2.save()
+        // await task3.save()
         const tasks = await Task.all()
         return view.render('tasklist', {
             tasks: tasks.toJSON()
